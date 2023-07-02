281bf7ec5e2a2d4f7514a45ea4887c5ea42180de
Mohamed <mohamedmahmoudhassan2015@gmail.com>
2023-07-02 21:22:12 +0300

    add swiper

--- js/main.js
+++ js/main.js
@@ -49,3 +49,126 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+// **************************
+// **************************
+// **************************
+// **************************
+
+// main_swiper (7)
+const main_swiper = new Swiper('.main-slider .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+
+main_swiper.on('slideChange', function () {
+  new WOW().init()
+})
+
+const testi_swiper = new Swiper('.testimonial .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
+const adv_swiper = new Swiper('.advertisment .swiper', {
+  direction: 'horizontal',
+  loop: true,
+  autoplay: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 40,
+  freeMode: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    992: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
+
+var uluru = { lat: 24.774265, lng: 46.738586 }
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: uluru,
+  })
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    draggable: false,
+  })
+}
+
+// *********************************
+
+ filterSelection('all')
+function filterSelection(c) {
+  var x, i
+  x = document.getElementsByClassName('column');
+  //if (c == 'all') c = ''
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], 'show')
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], 'show')
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2
+  arr1 = element.className.split(' ')
+  arr2 = name.split(' ')
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += ' ' + arr2[i]
+    }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2
+  arr1 = element.className.split(' ')
+  arr2 = name.split(' ')
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1)
+    }
+  }
+  element.className = arr1.join(' ')
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById('myBtnContainer')
+var btns = btnContainer.getElementsByClassName('button')
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', function () {
+    var current = document.getElementsByClassName('active')
+    current[0].className = current[0].className.replace(' active', '')
+    this.className += ' active'
+  })
+}
+// **************************
